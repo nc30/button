a2698c8dfc9a92aed6fa5114f38fe7e8c2835a6f
Asahi Himura <himura@nitolab.com>
2019-04-03 01:06:57 +0900

    code

--- src/main.ino
+++ src/main.ino
@@ -4,6 +4,7 @@ int POWER = 10;
 int STANDBY = 16;
 int KEY_ACTIVATE = 2;
 int ENTER = 3;
+bool press = false;
 
 void setup() {
   pinMode(KEY_ACTIVATE, INPUT_PULLUP);
@@ -17,8 +18,13 @@ void setup() {
 void loop() {
   if(digitalRead(KEY_ACTIVATE) == LOW) {
     digitalWrite(STANDBY, LOW);
-      Keyboard.write(0x61);
-      Keyboard.write(0x0a);
+    if (digitalRead(ENTER) == LOW) {
+      delay(10);
+      if (digitalRead(ENTER) == LOW) {
+        Keyboard.write(0x0a);
+        delay(1000);
+      }
+    }
   }else{
     digitalWrite(STANDBY, LOW);
   }
